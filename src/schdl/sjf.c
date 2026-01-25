ea26e59bb5592d63ad08456ef28bae35f60b7920
JhonaRaida <jonathan.mb0405@gmail.com>
2026-01-24 21:06:50 -0600

    Entrega archivo sjf.c

--- src/schdl/sjf.c
+++ src/schdl/sjf.c
@@ -6,9 +6,36 @@
  * ============================================================ */
 void sjf_schedule(Process p[], int n)
 {
-    (void)p;
-    (void)n;
-    /* TODO: Implement SJF scheduling algorithm here */
+    int time = 0;
+    int completed = 0;
+
+    while (completed < n) {
+        int idx = -1;
+        int min_burst = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            if (p[i].arrival_time <= time &&
+                !p[i].completed &&
+                p[i].burst_time < min_burst) {
+
+                min_burst = p[i].burst_time;
+                idx = i;
+            }
+        }
+
+        if (idx == -1) {
+            time++;
+        } else {
+            time += p[idx].burst_time;
+            p[idx].completed = 1;
+            completed++;
+
+            p[idx].turnaround_time =
+                time - p[idx].arrival_time;
+            p[idx].waiting_time =
+                p[idx].turnaround_time - p[idx].burst_time;
+        }
+    }
 }
 
 /* ============================================================
