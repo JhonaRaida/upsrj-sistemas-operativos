598783f06a33741a71dd88f68796f1bd8f1952ec
JhonaRaida <jonathan.mb0405@gmail.com>
2026-01-24 20:43:02 -0600

    Implementación del planificador Round Robin

--- src/schdl/rr.c
+++ src/schdl/rr.c
@@ -6,10 +6,37 @@
  * ============================================================ */
 void rr_schedule(Process p[], int n, int quantum)
 {
-    (void)p;
-    (void)n;
-    (void)quantum;
-    /* TODO: Implement RR scheduling algorithm here */
+    int time = 0;
+    int completed = 0;
+
+    while (completed < n) {
+        int executed = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (p[i].arrival_time <= time && p[i].remaining_time > 0) {
+                executed = 1;
+
+                if (p[i].remaining_time > quantum) {
+                    time += quantum;
+                    p[i].remaining_time -= quantum;
+                } else {
+                    time += p[i].remaining_time;
+                    p[i].remaining_time = 0;
+                    p[i].completed = 1;
+                    completed++;
+
+                    p[i].turnaround_time = time - p[i].arrival_time;
+                    p[i].waiting_time =
+                        p[i].turnaround_time - p[i].burst_time;
+                }
+            }
+        }
+
+        if (!executed) {
+            time++;
+        }
+    }
 }
 
 /* ============================================================
