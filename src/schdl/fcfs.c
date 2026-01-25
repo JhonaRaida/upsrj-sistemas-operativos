e884a44792d17a3253a06622df69330647437d0c
JhonaRaida <jonathan.mb0405@gmail.com>
2026-01-24 21:46:35 -0600

    Archivo fcfs.c

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -6,40 +6,39 @@
  * ============================================================ */
 void fcfs_schedule(Process p[], int n)
 {
-    (void)p;
-    (void)n;
-    /* TODO: Implement FCFS scheduling algorithm here */
     int time = 0;
+    Process result[n];
+    int k = 0;
 
-    // 1. Ordenar procesos por arrival_time (FCFS real)
+    // Ordenar por arrival_time y id
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (p[i].arrival_time > p[j].arrival_time) {
-                Process temp = p[i];
+            if (p[j].arrival_time < p[i].arrival_time ||
+               (p[j].arrival_time == p[i].arrival_time &&
+                p[j].id < p[i].id)) {
+
+                Process tmp = p[i];
                 p[i] = p[j];
-                p[j] = temp;
+                p[j] = tmp;
             }
         }
     }
 
-    // 2. Ejecutar procesos en orden de llegada
     for (int i = 0; i < n; i++) {
 
-        // Si el proceso llega después, el CPU espera
-        if (time < p[i].arrival_time) {
+        if (time < p[i].arrival_time)
             time = p[i].arrival_time;
-        }
 
-        // Waiting Time = tiempo actual - arrival time
         p[i].waiting_time = time - p[i].arrival_time;
-
-        // Ejecutar proceso completo
         time += p[i].burst_time;
-
-        // Turnaround Time = tiempo final - arrival time
         p[i].turnaround_time = time - p[i].arrival_time;
 
-        p[i].completed = 1;
+        result[k++] = p[i];
+    }
+
+    // Copiar orden final
+    for (int i = 0; i < n; i++) {
+        p[i] = result[i];
     }
 }
 
